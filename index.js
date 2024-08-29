57cf35d39066ee195a1404b0ffc3ceb84908c00b
jianghua1 <719584530@qq.com>
2024-08-29 14:52:39 +0800

    第一次提交

---
+++ index.js
@@ -0,0 +1,17 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('node:path')
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      sandbox: false,
+      preload: path.join(__dirname, 'preload.js'),
+    }
+  })
+  //窗口对象win
+  win.loadFile('index.html');
+  win.webContents.openDevTools();
+}
+app.whenReady().then(createWindow)
